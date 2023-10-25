36257f2fa7e95af94e9c5b27cc702f262b3a45ba
Colin Stimson <89496259+sinapticgenesis@users.noreply.github.com>
2023-10-25 14:49:45 -0500

    Update main.py
    Initial program layout and definition of getValue function

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+def main():
+  list = getData()
 
+
+
+def getData():
+  print('Enter String 1')
+  s1 = input('String 1 value: ')
+  print('Enter string 2')
+  s2 = input('String 2 value: ')
+  return s1,s2
+
+
+
+
+def testForAnagram():
+  pass
+
+
+
+def testForPalindrome():
+  pass
+
+
+main() #call main function.
